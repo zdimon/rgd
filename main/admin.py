5af4f6606f1f908b07d9e4a6ee439a5837cc7110
zdimon <zdimon@example.com>
2018-09-18 15:14:08 +0300

    auto commit

--- main/admin.py
+++ main/admin.py
@@ -4,3 +4,28 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+   
+admin.site.register(Theme, ThemeAdmin)
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+   
+admin.site.register(Journal, JournalAdmin)
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+   
+admin.site.register(Issue, IssueAdmin)
+
+
+class TopJournalAdmin(admin.ModelAdmin):
+    list_display = ('journal_name','date')
+   
+admin.site.register(TopJournal, TopJournalAdmin)
+
